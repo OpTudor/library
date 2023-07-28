e7e24e41804efd8c79f98823b97bce3ac54b9317
OpTudor <133231902+OpTudor@users.noreply.github.com>
2023-07-28 22:03:55 +0300

    Add a form to add the books
    + you can see the books

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const submitButton = document.getElementById("addBook");
+const bookshelf = document.getElementById("bookshelf");
 let library = [];
 
 function book(name, author, pages, read) {
@@ -11,12 +13,39 @@ function book(name, author, pages, read) {
 }
 
 function addBook() {
-  let name, author, pages, read, book1;
-  name = prompt("Name?", name);
-  author = prompt("Author?", author);
-  pages = prompt("Pages?", pages);
-  read = prompt("Read?", read);
-  book1 = new book(name, author, pages, read);
-  library.push(book1);
+  let name = document.getElementById("name").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read = document.getElementById("read").value;
+  let theBook = new book(name, author, pages, read);
+  library.push(theBook);
   console.log(library);
 }
+
+function showBooks() {
+  library.forEach((book) => {
+    let cover = document.createElement("div");
+    bookshelf.appendChild(cover);
+    let bookName = document.createElement("div");
+    bookName.classList.add("name");
+    bookName.textContent = book.name;
+    cover.appendChild(bookName);
+    let bookAuthor = document.createElement("div");
+    bookAuthor.classList.add("author");
+    bookAuthor.textContent = book.author;
+    cover.appendChild(bookAuthor);
+    let bookPages = document.createElement("div");
+    bookPages.classList.add("pages");
+    bookPages.textContent = book.pages;
+    cover.appendChild(bookPages);
+    let bookRead = document.createElement("div");
+    bookRead.classList.add("read");
+    bookRead.textContent = book.read;
+    cover.appendChild(bookRead);
+  });
+}
+
+submitButton.addEventListener("click", () => {
+  addBook();
+  showBooks();
+});
