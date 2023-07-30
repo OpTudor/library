8278d157e376cbce9b8ac7721ba846ef3f27aa17
OpTudor <133231902+OpTudor@users.noreply.github.com>
2023-07-30 22:45:12 +0300

    Finished the project
    Added:
    -the ability to delete the books, toggle the read status
    -background blur when the form is toggled
    -proper styling to everything
    -remove button on the books
    Fixed:
    -not being able to click away from the form if anything was underneath

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bookshelf = document.getElementById("bookshelf");
 const addButton = document.getElementById("add");
 const modal = document.getElementById("modal");
 const form = document.querySelector("form");
+const header = document.getElementById("header");
+const overlay = document.getElementById("overlay");
 let library = [];
 
 function book(name, author, pages, read) {
@@ -22,10 +24,11 @@ function addBook() {
   let read = document.getElementById("read").value;
   let theBook = new book(name, author, pages, read);
   library.push(theBook);
-  console.log(library);
 }
 
 function showBooks() {
+  let counter = 0;
+  clearBookshelf();
   library.forEach((book) => {
     let cover = document.createElement("div");
     cover.classList.add("cover");
@@ -40,30 +43,70 @@ function showBooks() {
     cover.appendChild(bookAuthor);
     let bookPages = document.createElement("div");
     bookPages.classList.add("pages");
-    bookPages.textContent = book.pages;
+    bookPages.textContent = book.pages + " pages";
     cover.appendChild(bookPages);
-    let bookRead = document.createElement("div");
-    bookRead.classList.add("read");
-    bookRead.textContent = book.read;
+    let bookRead = document.createElement("button");
+    if (book.read == "true") {
+      bookRead.classList.add("read");
+      bookRead.textContent = "Read";
+    } else {
+      bookRead.classList.add("notRead");
+      bookRead.textContent = "Not Read";
+    }
+    bookRead.classList.add("checkRead");
+    bookRead.id = counter;
+    bookRead.addEventListener("click", () => {
+      changeStatus(bookRead);
+    });
+    counter++;
     cover.appendChild(bookRead);
+    let removeButton = document.createElement("button");
+    removeButton.classList.add("remove");
+    removeButton.textContent = "Remove";
+    removeButton.addEventListener("click", () => {
+      removeBook(bookRead);
+    });
+    cover.appendChild(removeButton);
   });
 }
 
 submitButton.addEventListener("click", () => {
   addBook();
   showBooks();
+  modal.style.display = "none";
+  overlay.style.display = "none";
+  toggleBlur();
 });
 addButton.addEventListener("click", () => {
   modal.style.display = "flex";
+  overlay.style.display = "flex";
+  toggleBlur();
 });
 
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    overlay.style.display = "none";
+    toggleBlur();
   }
 };
-let book1 = new book("Game of Thrones", "Michael Jackson", "999", true);
-let book2 = new book("Game of Thrones", "Michael Jordan", "999", false);
-library.push(book1);
-library.push(book2);
-showBooks();
+function toggleBlur() {
+  bookshelf.classList.toggle("blur");
+  header.classList.toggle("blur");
+  addButton.classList.toggle("blur");
+}
+function changeStatus(button) {
+  library[button.id].readBook();
+  button.classList.toggle("read", library[button.id].read);
+  button.classList.toggle("notRead", !library[button.id].read);
+  button.textContent = library[button.id].read ? "Read" : "Not Read";
+}
+
+function removeBook(button) {
+  library.splice(button.id, 1);
+  showBooks();
+}
+
+function clearBookshelf() {
+  bookshelf.textContent = "";
+}
